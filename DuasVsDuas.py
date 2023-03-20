079e9090b73dfe397046fbf6b4cf5c14a32dce96
Nayara Nasaria <85569222+NayaraNasaria@users.noreply.github.com>
2023-03-20 20:38:40 -0300

    Update DuasVsDuas.py

--- DuasVsDuas.py
+++ DuasVsDuas.py
@@ -3,10 +3,7 @@ menina1Dupla2 = int(input())
 menina2Dupla1 = int(input())
 menina2Dupla2 = int(input())
 
-if menina1Dupla1 == menina1Dupla2 or menina1Dupla2 == menina2Dupla1:
-    print("empate")
-
-elif menina1Dupla1 > menina1Dupla2 and menina1Dupla1 >= menina2Dupla1 and menina1Dupla1 > menina2Dupla2:
+if menina1Dupla1 > menina1Dupla2 and menina1Dupla1 >= menina2Dupla1 and menina1Dupla1 > menina2Dupla2:
     print(menina1Dupla1)
 
 elif menina1Dupla2 > menina1Dupla1 and menina1Dupla2 > menina2Dupla1 and menina1Dupla2 >= menina2Dupla2:
@@ -17,4 +14,5 @@ elif menina2Dupla1 > menina1Dupla1 and menina2Dupla1 > menina1Dupla2 and menina2
 
 elif menina2Dupla2 > menina1Dupla1 and menina2Dupla2 > menina1Dupla2 and menina2Dupla2 > menina2Dupla1:
     print(menina2Dupla2)
-
+else:
+    print("empate")
